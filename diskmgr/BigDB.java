757829bb417ff8d45342634acdfeb29a7f1f3892
Amitabh Das <amitab.das@outlook.com>
2020-03-22 22:11:55 -0700

    More

---
+++ diskmgr/BigDB.java
@@ -0,0 +1,50 @@
+package diskmgr;
+
+import java.io.IOException;
+
+import BigT.*;
+import btree.AddFileEntryException;
+import btree.ConstructPageException;
+import btree.GetFileEntryException;
+import heap.HFBufMgrException;
+import heap.HFDiskMgrException;
+import heap.HFException;
+import heap.InvalidSlotNumberException;
+import heap.InvalidTupleSizeException;
+
+public class BigDB extends DB
+{
+    public bigT bigt;
+    int _type;
+
+    public BigDB(int type)
+    {
+        _type = type;
+    }
+
+    public void initBigT(String name) throws 
+        IOException,
+        HFDiskMgrException,
+        HFBufMgrException,
+        HFException,
+        ConstructPageException,
+        GetFileEntryException,
+        AddFileEntryException 
+    {
+        bigt = new bigT(name+_type, _type);
+    }
+
+    public void insertMap(byte[] mapbytearray) throws Exception
+    {
+        bigt.insertMap(mapbytearray);
+    }
+
+    public int getMapCnt()throws HFBufMgrException,
+        IOException,
+        HFDiskMgrException,
+        InvalidSlotNumberException,
+        InvalidTupleSizeException 
+    {
+        return bigt.getMapCnt();
+    }
+} 
--- driver/BatchInsert.java
+++ driver/BatchInsert.java
@@ -16,54 +16,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BatchInsert {
-    //insert your data file in /data folder before running the make command, to include the data file in classpath.
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("DATAFILENAME(ensure that it is placed in /driver/data/):");
-        String fileName = br.readLine();
-        System.out.print("TYPE(1,2,3,4,5):");
-        int type = Integer.parseInt(br.readLine());
-        System.out.print("BIGTABLENAME:");
-        String bigtName = br.readLine();
-        URL url = BatchInsert.class.getResource("/data/".concat(fileName));
-        File csvFile = new File(url.getPath());
-        BufferedReader read = new BufferedReader(new FileReader(csvFile));
-        String line,rec[];
-        Map temp;
-        List<Map> mapLs = new ArrayList<>();
-        int c=0;
-        String dbpath = "D:\\minibase_db\\"+"hf"+System.getProperty("user.name")+".minibase-db";
-        SystemDefs sysdef = new SystemDefs(dbpath,100000,100,"Clock");
-        bigT b1 = new bigT(bigtName,type);
-        long timeStart = System.currentTimeMillis();
-        while ((line = read.readLine()) != null) {
-            rec = line.split(",");
-            temp = new Map();
-            if(c==0) {
-                temp.setRowLabel(rec[0].substring(1));
-            }
-            else {
-                temp.setRowLabel(rec[0]);
-            }
-            temp.setColumnLabel(rec[1]);
-            temp.setTimeStamp(Integer.parseInt(rec[3]));
-            temp.setValue(rec[2]);
-            b1.insertMap(temp.getMapByteArray());
-            //mapLs.add(temp);
-            c++;
-            System.out.println(c);
-        }
-        System.out.println(b1.getMapCnt());
-        System.out.println(b1.getRowCnt());
-        System.out.println(b1.getColumnCnt());
-        System.out.println("Time taken"+(System.currentTimeMillis()-timeStart));
-        //System.out.println(mapLs.size());
-        //Map mapArr[] = mapLs.toArray(new Map[mapLs.size()]);
-        //System.out.println(mapArr[0].getRowLabel());
-        //insert code for batch insert
-        //insert code for setting up bigt
-    }
-
     //perform the batchinsertion but not in a main method
     public static void batchinsert(String fileName, int type, String bigtName, int numbuf) throws Exception
     {
@@ -97,7 +49,6 @@ public class BatchInsert {
         System.out.println("Map Count : "+b1.getMapCnt());
         b1.btf.close();
         b1.btfTS.close();
-        //System.out.println(b1.getColumnCnt());
-        //System.out.println(b1.getRowCnt());
+        read.close();
     }
 }
